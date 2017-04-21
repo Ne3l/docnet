0bdcf9e2afa432da00beb49b78233710da1723e4
Nacho Lopez <ncho.lopez@gmail.com>
2017-04-21 17:04:33 +0200

    Updated Chrome version, Electron updater

--- app/main.js
+++ app/main.js
@@ -1,39 +1,47 @@
 const electron = require('electron');
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
-const path = require('path')
-const url = require('url')
-let mainWindow
+const path = require('path');
+const url = require('url');
+let mainWindow;
 
-function createWindow () {
-  mainWindow = new BrowserWindow({width: 800, height: 600, title:'Docnet', backgroundColor: '#2c363f',show: false})
-  mainWindow.maximize();
-  mainWindow.setMenu(null)
+function createWindow() {
+    mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        title: 'Docnet',
+        backgroundColor: '#2c363f',
+        show: false
+    });
+    mainWindow.maximize();
+    mainWindow.setMenu(null);
 
-  mainWindow.loadURL(url.format({
-    pathname: path.join(__dirname, 'index.html'),
-    protocol: 'file:',
-    slashes: true
-  }))
-  
-  mainWindow.once('ready-to-show', function () {
-    mainWindow.show()
-  })
+    mainWindow.loadURL(
+        url.format({
+            pathname: path.join(__dirname, 'index.html'),
+            protocol: 'file:',
+            slashes: true
+        })
+    );
 
-  mainWindow.on('closed', function () {
-    mainWindow = null
-  })
+    mainWindow.once('ready-to-show', function() {
+        mainWindow.show();
+    });
+
+    mainWindow.on('closed', function() {
+        mainWindow = null;
+    });
 }
 
-app.on('ready', createWindow)
-app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
-})
+app.on('ready', createWindow);
+app.on('window-all-closed', function() {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
 
-app.on('activate', function () {
-  if (mainWindow === null) {
-    createWindow()
-  }
-})
+app.on('activate', function() {
+    if (mainWindow === null) {
+        createWindow();
+    }
+});
--- app/render.js
+++ app/render.js
@@ -1,33 +1,35 @@
-const {shell} = require('electron')
-const autoUpdater = require('electron').remote.require('electron-auto-updater').autoUpdater;
-const remote = require('electron').remote; 
+const { shell } = require('electron');
+const autoUpdater = require('electron').remote.require('electron-updater').autoUpdater;
+const remote = require('electron').remote;
 const Menu = remote.Menu;
-autoUpdater.addListener("update-available", function (event) {
-    console.log("A new update is available");
-    var element = document.getElementById("docnet");
-    element.outerHTML = "";
-    delete element;
-    paintSpinner("Descargando Actualización");
+autoUpdater.addListener('update-available', function(event) {
+    console.log('A new update is available');
+    var element = document.getElementById('docnet');
+    element.outerHTML = '';
+    paintSpinner('Descargando Actualización');
 });
-autoUpdater.addListener("update-downloaded", (event, releaseNotes, releaseName, releaseDate, updateURL) => {
-    console.log("A new update is ready to install", `Version ${releaseName} is downloaded and will be automatically installed on Quit`);
+autoUpdater.addListener('update-downloaded', (event, releaseNotes, releaseName, releaseDate, updateURL) => {
+    console.log(
+        'A new update is ready to install',
+        `Version ${releaseName} is downloaded and will be automatically installed on Quit`
+    );
     autoUpdater.quitAndInstall();
     return true;
-})
-autoUpdater.addListener("error", (error) => {
+});
+autoUpdater.addListener('error', error => {
     console.log(error);
 });
 autoUpdater.checkForUpdates();
 
-const webview = document.getElementById("docnet");
+const webview = document.getElementById('docnet');
 const loading = document.getElementById('loading');
-   
-webview.style.height = window.innerHeight + "px";
-window.onresize = function (event) {
-    webview.style.height = window.innerHeight + "px";
+
+webview.style.height = window.innerHeight + 'px';
+window.onresize = function(event) {
+    webview.style.height = window.innerHeight + 'px';
 };
 
-function paintSpinner(label){
+function paintSpinner(label) {
     let spinner = '<div class="ms-spinner">';
     spinner += '<div class="ms-spinner-circle"></div>';
     spinner += '<div class="ms-spinner-label">';
@@ -37,19 +39,20 @@ function paintSpinner(label){
     loading.innerHTML = spinner;
 }
 
-function login(){
-    var functionClick = "function(e){"
-    functionClick+= " if($('#recordarCredentials').prop('checked')){";
-    functionClick+= "localStorage.setItem('user',$('#inputUsuario').val());";
-    functionClick+= "localStorage.setItem('password',$('#inputPassword').val());";
-    functionClick+= "}}";
-    
+function login() {
+    var functionClick = 'function(e){';
+    functionClick += " if($('#recordarCredentials').prop('checked')){";
+    functionClick += "localStorage.setItem('user',$('#inputUsuario').val());";
+    functionClick += "localStorage.setItem('password',$('#inputPassword').val());";
+    functionClick += '}}';
+
     var mainFunction = "if($('body').hasClass('login')){";
-    mainFunction += "$('#frmLogin').append('<input type=\"checkbox\" id=\"recordarCredentials\" checked/> <span style=\"color:#fff;\"> Recordar usuario y contraseña</span>');";
+    mainFunction +=
+        '$(\'#frmLogin\').append(\'<input type="checkbox" id="recordarCredentials" checked/> <span style="color:#fff;"> Recordar usuario y contraseña</span>\');';
     mainFunction += "$('#inputUsuario').val(localStorage.getItem('user'));";
     mainFunction += "$('#inputPassword').val(localStorage.getItem('password'));";
-    mainFunction += "$('#btnAceptar').on('click'," + functionClick + ")";
-    mainFunction += "}";
+    mainFunction += "$('#btnAceptar').on('click'," + functionClick + ')';
+    mainFunction += '}';
     webview.executeJavaScript(mainFunction);
 }
 
@@ -57,38 +60,45 @@ onload = () => {
     var showSpinner = true;
     const loadstart = () => {
         if (showSpinner) {
-           paintSpinner("Cargando"); 
+            paintSpinner('Cargando');
         }
-    }
+    };
     const loadstop = () => {
         if (showSpinner) {
             showSpinner = false;
             loading.innerHTML = '';
         }
-        webview.insertCSS('::-webkit-scrollbar {background-color: #eee ;width: 0.8em} ::-webkit-scrollbar-thumb:window-inactive, ::-webkit-scrollbar-thumb {background: #48525e ;}');
+        webview.insertCSS(
+            '::-webkit-scrollbar {background-color: #eee ;width: 0.8em} ::-webkit-scrollbar-thumb:window-inactive, ::-webkit-scrollbar-thumb {background: #48525e ;}'
+        );
         login();
-    }
-    webview.addEventListener('did-start-loading', loadstart)
-    webview.addEventListener('did-stop-loading', loadstop)
-}
+    };
+    webview.addEventListener('did-start-loading', loadstart);
+    webview.addEventListener('did-stop-loading', loadstop);
+};
 
-webview.addEventListener('new-window', function (event) {
+webview.addEventListener('new-window', function(event) {
     shell.openExternal(event.url);
 });
 
 webview.addEventListener('context-menu', (e, props) => {
-    const menu = Menu.buildFromTemplate([, {
-        label: 'Copiar',
-        role: 'copy',
-    }, {
-        label: 'Pegar',
-        role: 'paste',
-    }, {
-        type: 'separator',
-    }, {
-        label: 'Seleccionar Todo',
-        role: 'selectall',
-    },
+    const menu = Menu.buildFromTemplate([
+        ,
+        {
+            label: 'Copiar',
+            role: 'copy'
+        },
+        {
+            label: 'Pegar',
+            role: 'paste'
+        },
+        {
+            type: 'separator'
+        },
+        {
+            label: 'Seleccionar Todo',
+            role: 'selectall'
+        }
     ]);
     menu.popup(remote.getCurrentWindow());
 });
