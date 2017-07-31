ce9c7bc63ad4cc763207e0671abc32c5b8270bf9
Nacho Lopez <ncho.lopez@gmail.com>
2017-07-31 12:52:06 +0200

    Add Priting.

--- app/main.js
+++ app/main.js
@@ -1,8 +1,8 @@
 const electron = require('electron');
-const app = electron.app;
-const BrowserWindow = electron.BrowserWindow;
+const { app, BrowserWindow, ipcMain, dialog } = electron;
 const path = require('path');
 const url = require('url');
+const fs = require('fs');
 let mainWindow;
 
 function createWindow() {
@@ -24,6 +24,8 @@ function createWindow() {
         })
     );
 
+    mainWindow.webContents.openDevTools();
+
     mainWindow.once('ready-to-show', function() {
         mainWindow.show();
     });
@@ -44,4 +46,33 @@ app.on('activate', function() {
     if (mainWindow === null) {
         createWindow();
     }
+});
+
+ipcMain.on('print-to-pdf', (e, args) => {
+    let html = args || '';
+    html = html.trim();
+
+    if (!html) return;
+    let savePath = dialog.showSaveDialog({});
+    if (!savePath) return;
+
+    let winPrint = new BrowserWindow({ show: false });
+
+    var file = 'data:text/html;charset=UTF-8,' + encodeURIComponent(html);
+    winPrint.loadURL(file);
+
+    winPrint.once('ready-to-show', function() {
+        winPrint.webContents.printToPDF({}, (err, data) => {
+            if (err) {
+                dialog.showErrorBox('Error', err);
+                return;
+            }
+            fs.writeFile(savePath, data, err => {
+                if (err) {
+                    dialog.showErrorBox('Error', err);
+                    return;
+                }
+            });
+        });
+    });
 });
